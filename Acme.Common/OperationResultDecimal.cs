21137524981b1b1a474ea805f32610b5ce75d4e4
Sean Newman <seangnewman@msn.com>
2018-09-30 07:45:20 -0700

    Add project files.

---
+++ Acme.Common/OperationResultDecimal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Common
+{
+    public class OperationResultDecimal
+    {
+
+        public OperationResultDecimal()
+        {
+
+        }
+
+        public OperationResultDecimal(decimal result, string message) :this()
+        {
+            this.Result = result;
+            this.Message = message;
+
+        }
+
+        public decimal Result { get; set; }
+        public string Message { get; set; }
+    }
+}
